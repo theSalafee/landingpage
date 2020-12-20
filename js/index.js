e06d727269aefe1ae333e635018d0269f0673d96
Naasir Salafee <naasirbush@icloud.com>
2020-12-20 15:50:02 -0800

    Hooked up row four toggle

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function () {
+    $(".corpFormationTab, .nonprofitFormationTab").hide();
+
+    $(".llcBtn, .corpBtn, .nonprofitBtn").bind("click", function () {
+        $(".corpFormationTab, .llcFormationTab, .nonprofitFormationTab").hide();
+
+        if ($(this).attr("class") == "llcBtn") {
+            $(".llcFormationTab").show();
+        } else if (($(this).attr("class") == "corpBtn")) {
+            $(".corpFormationTab").show();
+        } else if (($(this).attr("class") == "nonprofitBtn")) {
+            $(".nonprofitFormationTab").show();
+        }
+
+    });
+});
+;
