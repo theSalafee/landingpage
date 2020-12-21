6836a55f8ecdfdb82f8b0a922636abc4d5888bbb
Naasir Salafee <naasirbush@icloud.com>
2020-12-20 21:47:41 -0800

    Working on the accordion

--- js/index.js
+++ js/index.js
@@ -13,3 +13,14 @@ $(function () {
         }
     });
 });
+
+$("#accordion ul li").each(function () {
+  var trigger = $(this).find("a");
+  var siblings = $(this).siblings();
+  var acc_text = $(this).find(".accordion");
+  $(trigger).click(function () {
+    var visible_text = siblings.find(".accordion").filter(":visible");
+    $(acc_text).slideToggle();
+    $(visible_text).slideUp();
+  });
+});
