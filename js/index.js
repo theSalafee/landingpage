9aa9ac64f1de6117d4bfe81695ee0f3770d36763
Naasir Salafee <naasirbush@icloud.com>
2020-12-27 19:17:17 -0800

    Got the buttons working

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
-$(function () {
-    $(".corpFormationTab, .nonprofitFormationTab").hide();
+$(document).ready(
+    function () {
+        $(".corpFormationTab, .nonprofitFormationTab").hide();
 
-    $(".llcBtn, .corpBtn, .nonprofitBtn").bind("click", function () {
-        $(".corpFormationTab, .llcFormationTab, .nonprofitFormationTab").hide();
+        $(".llcBtn, .corpBtn, .nonprofitBtn").bind("click", function () {
+            $(".corpFormationTab, .llcFormationTab, .nonprofitFormationTab").hide();
+            $(".llcBtn, .corpBtn, .nonprofitBtn").removeClass("active");
+            $(this).addClass("active");
 
-        if ($(this).attr("class") == "llcBtn") {
-            $(".llcFormationTab").show();
-        } else if ($(this).attr("class") == "corpBtn") {
-            $(".corpFormationTab").show();
-        } else if ($(this).attr("class") == "nonprofitBtn") {
-            $(".nonprofitFormationTab").show();
-        }
+            if ($(this).attr("id") == "show_llcFormationTab") {
+                $(".llcFormationTab").show();
+            } else if ($(this).attr("id") == "show_corpFormationTab") {
+                $(".corpFormationTab").show();
+            } else if ($(this).attr("id") == "show_nonprofitFormationTab") {
+                $(".nonprofitFormationTab").show();
+            }
+        });
     });
-});
 
 
+
+
+
+
+
+
+
+
+
+// $("#accordion ul li").each(function () {
+//   var trigger = $(this).find("a");
+//   var siblings = $(this).siblings();
+//   var acc_text = $(this).find(".accordion");
+//   $(trigger).click(function () {
+//     var visible_text = siblings.find(".accordion").filter(":visible");
+//     $(acc_text).slideToggle();
+//     $(visible_text).slideUp();
+//   });
+// });
